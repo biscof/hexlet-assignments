95c7861d1dd018a6971205a3a281a3c1f6ec8b2a
@hexlet/cli <support@hexlet.io>
2023-04-25 15:34:32 +0000

    submit java-web-ru/deploy

---
+++ java-web-ru/deploy/src/main/java/exercise/servlet/CompaniesServlet.java
@@ -0,0 +1,47 @@
+package exercise.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.util.ArrayList;
+import java.util.List;
+import static exercise.Data.getCompanies;
+
+public class CompaniesServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+                throws IOException, ServletException {
+
+        // BEGIN
+        List<String> filtered;
+        List<String> companies = getCompanies();
+        String queryString = request.getQueryString();
+        PrintWriter pw = response.getWriter();
+
+        if (queryString!= null && queryString.contains("search")) {
+            String strToSearch = request.getParameter("search");
+            if (strToSearch.isEmpty()) {
+                companies.forEach(pw::println);
+            } else {
+                filtered = companies.stream()
+                        .filter(company -> company.contains(strToSearch))
+                        .toList();
+                if (filtered.isEmpty()) {
+                    pw.println("Companies not found");
+                } else {
+                    filtered.forEach(pw::println);
+                }
+            }
+        } else {
+            companies.forEach(pw::println);
+        }
+        pw.close();
+        // END
+    }
+}
