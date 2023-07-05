284315e9b0e524a2191bbb4373ec2eab4b90886b
@hexlet/cli <support@hexlet.io>
2023-07-05 19:28:23 +0100

    submit java-spring-ru/migrations

---
+++ java-spring-ru/migrations/src/main/java/exercise/controller/PeopleController.java
@@ -0,0 +1,41 @@
+package exercise.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/people")
+@RequiredArgsConstructor
+public class PeopleController {
+
+    private final JdbcTemplate jdbc;
+
+    @PostMapping(path = "")
+    public void createPerson(@RequestBody Map<String, Object> person) {
+        String query = "INSERT INTO person (first_name, last_name) VALUES (?, ?)";
+        jdbc.update(query, person.get("first_name"), person.get("last_name"));
+    }
+
+    // BEGIN
+    @GetMapping(path = "")
+    public List<Map<String, Object>> listPeople() {
+        String query = "SElECT * FROM person";
+        return jdbc.queryForList(query);
+    }
+
+    @GetMapping(path = "/{id}")
+    public Map<String, Object> showPerson(@PathVariable Integer id) {
+        String query = "SElECT * FROM person WHERE id=?";
+        return jdbc.queryForMap(query, id);
+    }
+    // END
+}
