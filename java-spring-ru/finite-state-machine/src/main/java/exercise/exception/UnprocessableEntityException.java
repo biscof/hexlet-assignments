46928b234c301095b2d2c9020dba83970c61850a
@hexlet/cli <support@hexlet.io>
2023-07-08 22:10:55 +0100

    submit java-spring-ru/finite-state-machine

---
+++ java-spring-ru/finite-state-machine/src/main/java/exercise/exception/UnprocessableEntityException.java
@@ -0,0 +1,11 @@
+package exercise.exception;
+
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus;
+
+@ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+public class UnprocessableEntityException extends RuntimeException {
+    public UnprocessableEntityException(String message) {
+        super(message);
+    }
+}
