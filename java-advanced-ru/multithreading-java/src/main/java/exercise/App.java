3bc236262f6c923c3476fc5d6b6ccf3c1a36416d
@hexlet/cli <support@hexlet.io>
2023-08-14 18:29:56 +0100

    submit java-advanced-ru/multithreading-java

--- java-advanced-ru/multithreading-java/src/main/java/exercise/App.java
+++ java-advanced-ru/multithreading-java/src/main/java/exercise/App.java
@@ -1,12 +1,8 @@
 package exercise;
 
 import java.util.Map;
-import java.util.logging.Logger;
-import java.util.logging.Level;
 
 class App {
-    private static final Logger LOGGER = Logger.getLogger("AppLogger");
-
     // BEGIN
 
     public static Map<String, Integer> getMinMax(int[] numbers) {
